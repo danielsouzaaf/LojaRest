3032720fe449ca9d6e99a17d43cabfe8097ede7a
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2016-06-27 16:04:31 -0300

    UHUL

---
+++ LojaAppRest/VeiculoWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LojaAppRest
+{
+    /// <summary>
+    /// Interaction logic for VeiculoWindow.xaml
+    /// </summary>
+    public partial class VeiculoWindow : Window
+    {
+        public VeiculoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LojaRest/Controllers/VeiculoController.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaRest.Controllers
+{
+    public class VeiculoController : ApiController
+    {
+        // GET: api/Veiculo
+        public IEnumerable<Models.Veiculo> Get()
+        {
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            var r = from v in dc.Veiculos select v;
+            return r.ToList();
+        }
+
+        // POST: api/Veiculo
+        public void Post([FromBody]string value)
+        {
+
+            List<Models.Veiculo> x = JsonConvert.DeserializeObject
+            <List<Models.Veiculo>>(value);
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            dc.Veiculos.InsertAllOnSubmit(x);
+            dc.SubmitChanges();
+        }
+
+        // PUT: api/Veiculo/5
+        public void Put(int id, [FromBody]string value)
+        {
+            Models.Veiculo x = JsonConvert.DeserializeObject
+           <Models.Veiculo>(value);
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            Models.Veiculo vei = (from f in dc.Veiculos
+                                     where f.id == id
+                                     select f).Single();
+            vei.Ano = x.Ano;
+            vei.DataCompra = x.DataCompra;
+            vei.DataVenda = x.DataVenda;
+            vei.Modelo = x.Modelo;
+            vei.PrecoVenda = x.PrecoVenda;
+            vei.ValorCompra = x.ValorCompra;
+            vei.ValorVenda = x.ValorVenda;
+            dc.SubmitChanges();
+        }
+
+        // DELETE: api/Veiculo/5
+        public void Delete(int id)
+        {
+            Models.LojaDataContext dc = new Models.LojaDataContext();
+            Models.Veiculo vei = (from f in dc.Veiculos
+                                     where f.id == id
+                                     select f).Single();
+            dc.Veiculos.DeleteOnSubmit(vei);
+            dc.SubmitChanges();
+        }
+    }
+}
